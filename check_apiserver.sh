a7183901a16f2bc3c4b7c0c1d042ab8eb567d60b
Yarikscode <148035045+Yarikscode@users.noreply.github.com>
2025-04-02 12:10:07 +0700

    Update check_apiserver.sh
    final checkapiscript

--- check_apiserver.sh
+++ check_apiserver.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
 APISERVER_VIP=192.168.31.100
 APISERVER_DEST_PORT=6443
 
-errorExit() { 
-	echo "* * * $*" 1>&2
-	exit 1
+errorExit() {
+        echo "* * * $*" 1>&2
+        exit 1
 }
 
-curl --silent --max-time 2 --insecure https://localhost:${APISERVER_DEST_PORT}/ -o /dev/null || errorExit "Error GET https://localhost:${APISERVER_DEST_PORT}/"
+# Проверяем только если VIP на этом хосте
 if ip addr | grep -q ${APISERVER_VIP}; then
-	curl --silent --max-time 2 --insecure https://${APISERVER_VIP}:${APISERVER_DEST_PORT}/ -o /dev/null || errorExit "Error GET https://${APISERVER_VIP}:${APISERVER_DEST_PORT}/"
+        curl --silent --max-time 2 --insecure https://${APISERVER_VIP}:${APISERVER_DEST_PORT}/ -o /dev/null || errorExit "Error GET https://${APISERVER_VIP}:${APISERVER_DEST_PORT}/"
 fi
+
+exit 0
